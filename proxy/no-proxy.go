425296fce777856eb3c6b3d584d907494bc0c9ae
Aamir Maniar <aamironline@gmail.com>
2024-10-01 23:30:45 +0530

    refactor: Remove unused Timeout field in NoProxy struct

--- proxy/no-proxy.go
+++ proxy/no-proxy.go
@@ -8,7 +8,6 @@ import (
 
 // NoProxy represents a direct connection without any proxy.
 type NoProxy struct {
-	Timeout time.Duration
 }
 
 // Apply configures the transport for a direct connection.
@@ -17,7 +16,6 @@ func (np *NoProxy) Apply(transport *http.Transport) error {
 
 	// Configure DialContext with a timeout.
 	dialer := &net.Dialer{
-		Timeout:   np.Timeout,
 		KeepAlive: 30 * time.Second,
 	}
 	transport.DialContext = dialer.DialContext
